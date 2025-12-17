906d308827c2ed5ade90f7b33716ea80af4b5770
sanif <sanifhimani92@gmail.com>
2025-12-16 21:04:36 -0500

    feat: add code block titles (#36)
    * feat: add code block titles
    
    * fix: rubocop

---
+++ lib/docyard/components/code_block_icon_detector.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require_relative "../language_mapping"
+
+module Docyard
+  module Components
+    module CodeBlockIconDetector
+      MANUAL_ICON_PATTERN = /^:([a-z0-9-]+):\s*(.+)$/i
+
+      module_function
+
+      def detect(title, language)
+        return { title: nil, icon: nil, icon_source: nil } if title.nil?
+
+        if (match = title.match(MANUAL_ICON_PATTERN))
+          return {
+            title: match[2].strip,
+            icon: match[1],
+            icon_source: "phosphor"
+          }
+        end
+
+        icon, icon_source = auto_detect_icon(language)
+        { title: title, icon: icon, icon_source: icon_source }
+      end
+
+      def auto_detect_icon(language)
+        return [nil, nil] if language.nil?
+
+        if LanguageMapping.terminal_language?(language)
+          %w[terminal-window phosphor]
+        elsif (ext = LanguageMapping.extension_for(language))
+          [ext, "file-extension"]
+        else
+          %w[file phosphor]
+        end
+      end
+    end
+  end
+end
