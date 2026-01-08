4161a072a8eec391e9de96f3a5a93967560e979d
Sanif Himani <sanifhimani92@gmail.com>
2026-01-08 11:19:15 -0500

    feat(sidebar): integrate _sidebar.yml into SidebarBuilder

--- lib/docyard/navigation/sidebar_builder.rb
+++ lib/docyard/navigation/sidebar_builder.rb
@@ -5,6 +5,7 @@ require_relative "sidebar/title_extractor"
 require_relative "sidebar/tree_builder"
 require_relative "sidebar/renderer"
 require_relative "sidebar/config_parser"
+require_relative "sidebar/local_config_loader"
 
 module Docyard
   class SidebarBuilder
@@ -28,14 +29,16 @@ module Docyard
 
     def build_tree
       if config_sidebar_items?
-        build_tree_from_config
+        build_tree_from_config(config_sidebar_items)
+      elsif local_config_items?
+        build_tree_from_config(local_config_items)
       else
         build_tree_from_filesystem
       end
     end
 
-    def build_tree_from_config
-      config_parser.parse.map(&:to_h)
+    def build_tree_from_config(items)
+      config_parser(items).parse.map(&:to_h)
     end
 
     def build_tree_from_filesystem
@@ -53,9 +56,21 @@ module Docyard
       config.sidebar&.items || []
     end
 
-    def config_parser
-      @config_parser ||= Sidebar::ConfigParser.new(
-        config_sidebar_items,
+    def local_config_items?
+      local_config_items&.any?
+    end
+
+    def local_config_items
+      @local_config_items ||= local_config_loader.load
+    end
+
+    def local_config_loader
+      @local_config_loader ||= Sidebar::LocalConfigLoader.new(docs_path)
+    end
+
+    def config_parser(items)
+      Sidebar::ConfigParser.new(
+        items,
         docs_path: docs_path,
         current_path: current_path
       )
