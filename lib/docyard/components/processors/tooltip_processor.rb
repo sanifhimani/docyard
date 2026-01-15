103a7f135cc0e9c5a509d5143d6ed6cfe4529db2
Sanif Himani <sanifhimani92@gmail.com>
2026-01-15 12:18:45 -0500

    feat(components): add tooltip processor with inline syntax

---
+++ lib/docyard/components/processors/tooltip_processor.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require_relative "../base_processor"
+
+module Docyard
+  module Components
+    module Processors
+      class TooltipProcessor < BaseProcessor
+        TOOLTIP_PATTERN = /:tooltip\[([^\]]+)\]\{([^}]+)\}/
+        self.priority = 6
+
+        def preprocess(content)
+          content.gsub(TOOLTIP_PATTERN) do |_match|
+            term = ::Regexp.last_match(1)
+            attributes = parse_attributes(::Regexp.last_match(2))
+            build_tooltip_tag(term, attributes)
+          end
+        end
+
+        private
+
+        def parse_attributes(attr_string)
+          attributes = {}
+          attr_string.scan(/(\w+)="([^"]*)"/) do |key, value|
+            attributes[key.to_sym] = value
+          end
+          attributes
+        end
+
+        def build_tooltip_tag(term, attributes)
+          description = escape_html(attributes[:description] || "")
+          link = attributes[:link]
+          link_text = attributes[:link_text] || "Learn more"
+
+          data_attrs = %(data-description="#{description}")
+          data_attrs += %( data-link="#{escape_html(link)}") if link
+          data_attrs += %( data-link-text="#{escape_html(link_text)}") if link
+
+          %(<span class="docyard-tooltip" #{data_attrs}>#{term}</span>)
+        end
+
+        def escape_html(text)
+          text.to_s
+            .gsub("&", "&amp;")
+            .gsub("<", "&lt;")
+            .gsub(">", "&gt;")
+            .gsub('"', "&quot;")
+        end
+      end
+    end
+  end
+end
